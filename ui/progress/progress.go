36b372f9627d07d19829aad61a0ca0d2952f146b
Endri Gjiri <egjiri@gmail.com>
2018-06-05 10:53:42 -0400

    Wrap the progress bar package with custom types

--- ui/progress/progress.go
+++ ui/progress/progress.go
@@ -1,33 +1,60 @@
 package progress
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/gosuri/uiprogress"
 )
 
-// BarManager manages the Progress of the bars
-var BarManager = uiprogress.New()
+// barManager manages the Progress of the bars
+var barManager = uiprogress.New()
 
-// Bar creates a prgoress bar with reasonable defaults, configurable count
+// Bar managest the state of a progres bar
+type bar struct {
+	bar *uiprogress.Bar
+}
+
+// NewBar creates a prgoress bar with reasonable defaults, configurable count
 // and action to display in the suffix of AppendFunc
-func Bar(count int, action string) (*uiprogress.Bar, error) {
+func NewBar(count int, action string) (*bar, error) {
 	if count < 1 {
-		return nil, fmt.Errorf("invalid progress bar count %v", count)
+		return nil, errors.New("invalid progress bar count")
 	}
 
-	bar := BarManager.AddBar(count)
+	b := barManager.AddBar(count)
+	progressBar := bar{b}
 	startTime := time.Now()
 	var duration string
-	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		if b.CompletedPercent() < 100 {
+	b.PrependFunc(func(b *uiprogress.Bar) string {
+		if progressBar.InProgress() {
 			duration = time.Since(startTime).Round(time.Second).String()
 		}
 		return duration
 	})
-	bar.AppendFunc(func(b *uiprogress.Bar) string {
+	b.AppendFunc(func(b *uiprogress.Bar) string {
 		return fmt.Sprintf("%.1f%% - %v %v/%v", b.CompletedPercent(), action, b.Current(), b.Total)
 	})
-	return bar, nil
+	return &progressBar, nil
+}
+
+// Incr increments the progress bar
+func (b *bar) Incr() bool {
+	return b.bar.Incr()
+}
+
+// InProgress returns whether the progress bar still has more to increment through
+func (b *bar) InProgress() bool {
+	return b.bar.CompletedPercent() < 100
+}
+
+// String returns the string representation of the progress bar
+func (b *bar) String() string {
+	return b.bar.String()
+}
+
+// Start begins the rendering of the progress bars
+func Start() {
+	barManager.Start()
 }
