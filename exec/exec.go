6b3da8ab06f7896d7d5301e1f93cf93bf7ffbbed
Endri Gjiri <egjiri@gmail.com>
2019-03-21 13:19:57 -0400

    Update the exec package Execute command to return errors

--- exec/exec.go
+++ exec/exec.go
@@ -1,6 +1,7 @@
 package exec
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
@@ -35,22 +36,26 @@ func ExecuteBash(command string) {
 }
 
 // Execute runs a command directly on the underlying OS
-func Execute(commands ...string) {
+func Execute(commands ...string) error {
 	args := buildCommandArgs(commands...)
 	name, err := exec.LookPath(args[0])
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	var procAttr os.ProcAttr
 	procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
 	p, err := os.StartProcess(name, args, &procAttr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	_, err = p.Wait()
+	ps, err := p.Wait()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	if !ps.Success() {
+		return errors.New("command execution failed")
+	}
+	return nil
 }
 
 func buildCommandArgs(commands ...string) []string {
